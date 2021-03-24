b0bd136ef87778b1eb3e3827934b42cbcbbc3ee5
thomasjk0831 <thomaskim08310@gmail.com>
2021-03-24 19:56:28 -0400

    add dashboard and sidebar basic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
+import Dashboard from "./Dashboard";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
-  return (
-    <>
-      {id}
-      <Login onIdSubmit={setId} />
-    </>
-  );
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
 }
 
 export default App;
--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -6,10 +6,15 @@ export default function useLocalStorage(key, initialValue) {
   const prefixedkey = PREFIX + key;
   const [value, setValue] = useState(() => {
     const jsonValue = localStorage.getItem(prefixedkey);
+    //set value to localstorage value if it exists and return
     if (jsonValue != null) return JSON.parse(jsonValue);
+    //if initialvalue is a function call 'setId' then set value
+    //to whatever that function returns
     if (typeof initialValue === "function") {
       return initialValue();
     } else {
+      //if initialvalue has not been called with a function,
+      //then just set value to be undefined
       return initialValue;
     }
   });
